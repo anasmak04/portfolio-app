f3277e4bd85e1f9a00618288d2e8c83ee2a1a615
anasmak04 <anasdev8@gmail.com>
2023-08-16 14:09:52 +0100

    added work section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <div className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
+      <form action="">
+
+      </form>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,31 +1,59 @@
 import React from "react";
 import { data } from "../data/data.js";
-import WorkImg from "../assets/workImg.jpeg";
-const Work = () => {
-  return (
-    <div name="work" className="w-full bg-[#0a192f] md:h-screen text-gray-300">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-      <div className="pb-8">
-      <p className="capitalize text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">work</p>
-        <p className="py-6">// check out some my recent work</p>
-      </div>
-      </div>
 
-      <div style={{backgroundImage: `url(${WorkImg})`}}
-       className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-        <div className="shadow-ld shadow-[#0f0c16] group container rounded-md flex justify-center items-center ms-auto content-div">
-  
-          <div className="">
-            <span></span>
-            <div className="">
-            <a href="/">
-              <button></button>
-            </a>
-            <a href="/">
-              <button></button>
-            </a>
+const Work = () => {
+  // projects file
+  const project = data;
+  //setProject(data);
+
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
+        </div>
+
+        {/* container for projects */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Gird Item */}
+          {project.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div "
+            >
+              {/* Hover effect for images */}
+              <div className="opacity-0 group-hover:opacity-100 ">
+                <span className="text-2xl font bold text-white tracking-wider ">
+                  {item.name}
+                </span>
+                <div className="pt-8 text-center ">
+                  {/* eslint-disable-next-line */}
+                  <a href={item.github} target="_blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Code
+                    </button>
+                  </a>
+                  {/* eslint-disable-next-line */}
+                  <a href={item.live} target="_blank">
+                    <button
+                      className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg"
+                    >
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
