c8d7ea8df7901afebc64fb1b897a0012b8d38d93
anasmak04 <anasdev8@gmail.com>
2023-08-17 22:32:41 +0100

    update work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { data } from "../data/data.js";
 
 const Work = () => {
-  // projects file
+  
   const project = data;
-  //setProject(data);
+ 
 
   return (
     <div name="work" className="w-full h-auto  text-gray-300 bg-[#0a192f]">
@@ -16,7 +16,7 @@ const Work = () => {
           <p className="py-6">// Check out some of my recent work</p>
         </div>
 
-        {/* container for projects */}
+       
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Gird Item */}
           {project.map((item, index) => (
@@ -26,13 +26,13 @@ const Work = () => {
               className="shadow-lg shadow-[#040c16] group container rounded-md 
               flex justify-center text-center items-center mx-auto content-div "
             >
-              {/* Hover effect for images */}
+      
               <div className="opacity-0 group-hover:opacity-100 ">
                 <span className="text-2xl font bold text-white tracking-wider ">
                   {item.name}
                 </span>
                 <div className="pt-8 text-center ">
-                  {/* eslint-disable-next-line */}
+                 
                   <a href={item.github} target="_blank">
                     <button
                       className="text-center rounded-lg px-4 py-3 m-2
@@ -41,7 +41,7 @@ const Work = () => {
                       Code
                     </button>
                   </a>
-                  {/* eslint-disable-next-line */}
+                  
                   <a href={item.live} target="_blank">
                     <button
                       className="text-center rounded-lg px-4 py-3 m-2
